76cf4a227120397139dedf716563e4533508a837
Texer0 <s5f14iulus69w@hotmail.com>
2025-04-08 21:09:14 -0300

    added members cards

--- src/components/common/CardMember.tsx
+++ src/components/common/CardMember.tsx
@@ -1,26 +1,15 @@
 type MemberProps = {
-  username: string;
-  branch: string;
-  children: React.ReactNode;
-};
+  username: string
+  branch?: string
+  children?: React.ReactNode
+}
 
 export const CardMember = ({ username, branch, children }: MemberProps) => {
   return (
     <div className="bg-white w-96 h-60 rounded-4xl p-5 pl-8 m-auto">
       <span className="text-black font-bold text-2xl">{username}</span>
-      <p className="text-black text-xl mt-2">{branch}</p>
+      <p className="text-black text-xl mt-2 mb-4">{branch}</p>
       <div className="flex flex-row flex-wrap [&>*]:mx-1">{children}</div>
     </div>
-  );
-};
-
-/* Use
-<CardMemeber username="Mr.Cube" branch="Full-Stack Developer">
-          <NodeJSIcon className="w-12"/>
-                <ReactIcon className="w-12"/>
-                <TailWindCSSIcon className="w-12"/>
-                <TypescriptIcon className="w-12"/>
-                <MySQLIcon className="w-24 h-12"/>
-                <PostgreSQLIcon className="w-12"/>
-          </CardMemeber>
-*/
+  )
+}
--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,7 +1,8 @@
-import { EmailIcon, InstagramIcon, NodeJSIcon } from "@/assets/icons/icons"
+import { DartIcon, EmailIcon, ExpressIcon, FlutterIcon, InstagramIcon, MySQLIcon, NestJSIcon, NodeJSIcon, PostgreSQLIcon, ReactIcon, TailWindCSSIcon, TypescriptIcon, ViteIcon } from "@/assets/icons/icons"
 import styles from "../../components/layout/styles/navbar.module.css"
 import ScrollToTop from '../../components/common/ScrollToTop'
 import { ProjectCard } from "@/components/common/ProjectCard"
+import { CardMember } from "@/components/common/CardMember"
 
 
 export const LandingPage = () => {
@@ -29,10 +30,56 @@ export const LandingPage = () => {
         </ProjectCard>
       </div>
     </div>
-    <div className="bg-red-400 h-screen">
+    <div className="bg-red-400 h-screen flex flex-col items-center gap-6 justify-center mb-96">
+      <div className="flex gap-10">
+      <CardMember username="Joe" branch="Full-Stack Developer">
+        <NodeJSIcon className="w-12 h-14"/>
+        <TypescriptIcon className="w-12"/>
+        <ReactIcon className="w-12 h-12"/>
+        <ViteIcon className="w-12 h-12"/>
+        <TailWindCSSIcon className="w-12"/>
+        <MySQLIcon className="w-24 h-12"/>
+        <ExpressIcon className="w-12 h-12"/>
+        <NestJSIcon className="w-12 h-12"/>
+      </CardMember>
+      <CardMember username="Méndez" branch="Back-end Developer">
+        <NodeJSIcon className="w-12 h-14"/>
+        <TypescriptIcon className="w-12"/>
+        <NestJSIcon className="w-12 h-12"/>
+        <DartIcon className="w-12 h-12"/>
+        <FlutterIcon className="w-12 h-12"/>
+      </CardMember>
+      <CardMember username="Melo" branch="Back-end Developer">
+        <NodeJSIcon className="w-12 h-14"/>
+        <TypescriptIcon className="w-12"/>
+        <NestJSIcon className="w-12 h-12"/>
+        <ReactIcon className="w-12 h-12"/>
+        <ViteIcon className="w-12 h-12"/>
+        <ExpressIcon className="w-12 h-12"/>
+      </CardMember>
+      <CardMember username="Mr.Cube" branch="Full-Stack Developer">
+        <NodeJSIcon className="w-12 h-14"/>
+        <ReactIcon className="w-12 h-12"/>
+        <ViteIcon className="w-12 h-12"/>
+        <TailWindCSSIcon className="w-12"/>
+        <MySQLIcon className="w-24 h-12"/>
+        <PostgreSQLIcon className="w-12 h-16"/>
+        <ExpressIcon className="w-12 h-12"/>
+        <TypescriptIcon className="w-12"/>
+      </CardMember>
+      </div>
+      <div className="flex gap-18">
+        <CardMember username="Mortem" branch="Full-Stack Developer">
+        </CardMember>
+        <CardMember username="Tomy" branch="">
+        </CardMember>
+        <CardMember username="Jota" branch="">
+        </CardMember>
+      </div>
     </div>
+    <div className="h-80"></div>
     <ScrollToTop/>
-    <footer id="contact" className="w-screen ">
+    <footer id="contact" className="w-screen">
       <div className="flex flex-row gap-x-16 justify-end mr-28 mb-20">
         <a href={`mailto:kodelux.ofc@gmail.com`} className="">
           <EmailIcon className="w-14"></EmailIcon>
