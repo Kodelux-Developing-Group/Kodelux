9199979eafb7123a5970fc4ae7391fd84d3d9596
Texer0 <s5f14iulus69w@hotmail.com>
2025-05-06 20:23:47 -0300

    Only production files

--- src/assets/images/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Sculpture } from "./Sculpture.png"
-export { default as PaniMarketKodeluxLogo } from "./panimarket_kodelux.png"
-export { default as KodeluxLogo } from "./kodelux.svg"
